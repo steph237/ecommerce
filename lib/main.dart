83690aa34ec31bf699e9d990c554fb3ccf655374
steph237 <stephaniewnkwatoh@gmail.com>
2023-03-04 04:47:54 +0100

    update: added the headset and the text

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,50 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Ecommerce app'),
-        ),
-        body: const DecoratedBox(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.cover),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'Setting Icon',
+              onPressed: () {},
+            ), //IconButton
+          ],
+          backgroundColor: Colors.black,
+          elevation: 50.0,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              shape: new CircleBorder(),
+            ),
           ),
-          child: Center(
-
         ),
-      ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg.jpg"),
+                fit: BoxFit.cover),
+          ),
+
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 400.0,
+                width: 400.0,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/headset.png'),
+
+                    ),
+
+                ),
+              ),
+              Container(
+                child: const Text('JBL T460BT'),
+              )
+            ],
+
+
+          ),
+        ),
       ),
     );
   }
